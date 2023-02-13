11003ae034b132b7b1a9cf08336735a8d77b7139
Ivan-Davila <arkangel_ivan@hotmail.com>
2023-02-13 14:20:33 -0600

    Master

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
+
 
 
 # Create your views here.
-@login_required(login_url='login/')
-def home(request):
-    return render(request,'home.html')
+
 
 @login_required
 def nuevo_registro(request):
     usuario = request.user
-    if usuario.perfilusuario.user_type != 'administrador':
+    if usuario.perfilusuario.user_type != 'administrador' or usuario.perfilusuario.user_type != 'distribuidor':
         return render(request,'home.html',{'error_message': 'No tienes permiso para acceder a esta funcion'})
     if request.method == 'POST':
         nombre = request.POST['nombre']
@@ -22,25 +20,10 @@ def nuevo_registro(request):
         
 
 
-def login_user(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request,user)
-            return redirect('home')
-        else:
-            # Muestra un mensaje de error si los credenciales no son válidos
-            return render(request, 'login.html', {'error_message': 'Datos incorrectos'})
-    return render(request,'login.html')
 
-def logout_user(request):
-    logout(request)
-    return redirect('home')
 
 def registro(request):
-    return redirect('home')
+    return render(request,'registro.html')
 
 @login_required
 def perfil(request):
