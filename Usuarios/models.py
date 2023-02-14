b5b83d77222e19f259657e39abe888bcbeeace35
Ivan-Davila <arkangel_ivan@hotmail.com>
2023-02-14 11:50:59 -0600

    Master

--- Usuarios/models.py
+++ Usuarios/models.py
@@ -1,16 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
-class TipoUsuario(models.Model):
-    tipo_usuario=models.CharField(max_length=30)
+# Create your models here.
 
 class PerfilUsuario(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    user_type = models.CharField(max_length=20, choices=[
-        ('administrator', 'Administrator'),
-        ('distribuidor', 'Distribuidor'),
-        ('cliente', 'Cliente'),
-    ])
+    user_type = models.CharField(max_length=20)
     creditos=models.PositiveIntegerField()
     registra=models.CharField(max_length=30)
